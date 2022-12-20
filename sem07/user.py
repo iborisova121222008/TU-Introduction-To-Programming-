888224c95dffeba0f290635c6c6d08d4ce1f7995
iborisova121222008 <116723766+iborisova121222008@users.noreply.github.com>
2022-12-20 19:26:14 +0200

    Create user.py

---
+++ sem07/user.py
@@ -0,0 +1,17 @@
+from errors import UserAlreadyOwnsAccount
+
+
+class User:
+    def __init__(self, names, egn) -> None:
+        self.names = names
+        self.egn = egn
+        self.accounts = []
+
+    def get_print(self):
+        return f"User({self.names}, {self.egn}, accounts: [{len(self.accounts)}])"
+
+    def add_account(self, account):
+        if account in self.accounts:
+            raise UserAlreadyOwnsAccount()
+
+        self.accounts.append(account)
