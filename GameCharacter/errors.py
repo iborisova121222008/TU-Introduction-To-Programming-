b9c5ddb1688572d54fae6f61eb4d0b2cffaf1b82
iborisova121222008 <116723766+iborisova121222008@users.noreply.github.com>
2022-12-21 15:49:48 +0200

    Create errors.py

---
+++ GameCharacter/errors.py
@@ -0,0 +1,23 @@
+class InvalidCommand(Exception):
+    def __init__(self, message="Enter a number between 1 and 5", *args: object) -> None:
+        super().__init__(message, *args)
+
+
+class CharacterExists(Exception):
+    def __init__(self, message="Please register new character", *args: object) -> None:
+        super().__init__(message, *args)
+
+
+class CharacterDoesNotExists(Exception):
+    def __init__(self, message="Character cannot be deleted", *args: object) -> None:
+        super().__init__(message, *args)
+
+
+class InvalidCharacterClass(Exception):
+    def __init__(self, message="Choose between Warrior, Mage, Priest, Rogue", *args: object) -> None:
+        super().__init__(message, *args)
+
+
+class InvalidUserData(Exception):
+    def __init__(self, message="Please enter valid data", *args: object) -> None:
+        super().__init__(message, *args)
